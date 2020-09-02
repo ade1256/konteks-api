4f98e226668da383498d25a9d50c5fb32fd2c6a1
Ade Prasetyo <adeprasetyo174@gmail.com>
2020-09-02 13:21:52 +0700

    change refresh token to jwdrive@mails

--- app/lib/getTokenGoogle.js
+++ app/lib/getTokenGoogle.js
@@ -7,7 +7,7 @@ const axios = require('axios')
 module.exports = () => {
   return new Promise(async resolve => {
     const url =
-      'https://accounts.google.com/o/oauth2/token?refresh_token=1//0giIime-wNZ2hCgYIARAAGBASNwF-L9IrDGe3ikVCF-En-WpalPRv6AEPKkiulInQjML7LtfChY-YCk0ZfHNYRXZ9KatXGU2wdVQ&grant_type=refresh_token&client_id=439829001690-99q55da2th9ndm8fqoajhkhr0plo05u9.apps.googleusercontent.com&client_secret=5qvEwxDL4d8698IVAugLpcGv'
+      'https://accounts.google.com/o/oauth2/token?refresh_token=1//0ghXfYG147f0TCgYIARAAGBASNwF-L9IrbbUFhHzi5bviwE4yQX3IVltHI6AP6smivF4Klp7zkNcbjoy34YNGycyskaxGBjonll0&grant_type=refresh_token&client_id=674453531566-lmb4jfo8j1f59sst1i2l4tr8nh2m9ohl.apps.googleusercontent.com&client_secret=cHn6sZmu6am6kpKJncakcz1w'
     axios
       .post(url)
       .then(resp => {
