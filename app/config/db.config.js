7fb27b63ffdce67e2714b8f032d00ba5abafab9e
Ade Prasetyo <adeprasetyo174@gmail.com>
2021-06-13 02:40:59 +0700

    Change pw db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   HOST: "127.0.0.1",
   USER: "cipi",
-  PASSWORD: "ymnjzmu1mjayzdu2yje3zmu5zdvmzgyw",
+  PASSWORD: "ntjjzjg5nwm1zwmwndixowviowyxmgnk",
   DB: "kontekstualkopi"
 };
